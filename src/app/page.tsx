db8526995b8b675a3bb1964db9910bfa5af377b1
Augusto Oliveira <augustocesarfmo@gmail.com>
2024-10-23 11:51:32 -0300

    feat: implementa renderização condicional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,43 @@
 "use client";
-
+import { useState } from "react";
 import { getAddress } from "../../get-address";
 
 export default function Home() {
-  let address = "Rua Teste";
+  const [address, setAddress] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   async function handleGetAddress() {
-    try {
-      const result = await getAddress("55330000");
-      address = result;
+    setLoading(true);
 
-      console.log(result);
+    try {
+      const result = await getAddress("52051000");
+      setAddress(result.logradouro);
+      // address = result;
+
+      console.log(result.logradouro);
     } catch (error) {
       console.log(error);
       alert("Ocorreu um erro ao obter o endereço.");
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div>
       <h1>Página Home</h1>
-      <span>Endereço: {address}</span>
-      <button
-        onClick={handleGetAddress}
-        className="px-5 py-3 bg-blue-700 text-white rounded-xl"
-      >
-        Obter endereço
-      </button>
+
+      <div className="flex flex-col gap-2">
+        <span>Endereço: {address}</span>
+        <button
+          onClick={handleGetAddress}
+          className={`${
+            loading && "opacity-30"
+          } w-fit px-5 py-3 bg-blue-700 text-white rounded-xl`}
+        >
+          {loading ? "Carregando..." : "Obter endereço"}
+        </button>
+      </div>
     </div>
   );
 }
